036fae6fb9fc6df676257b85ad2fd7a6bf1e5d1c
VVKhrushchev <vladius@bk.ru>
2017-06-28 00:53:53 +0300

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j;
-	public class Calculate {
-	public static void main(String[] args) {
+
+/**
+ * Class Calculate
+ *@ author Vladimir Khrushchev (vladius@bk.ru)
+ *@ since 27.06.17
+ *@ version 1
+ */
+
+
+public class Calculate {
+	/**
+	*@ Main
+	*@ param args - args
+	*/
+public static void main(String[] args) {
         System.out.println("Hello World");
                                        }
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vladimir Khrushchev (mailto:vladius@bk.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
